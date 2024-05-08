1803104ed0c6c7d81b65bbceb63670b878217fa3
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-05-08 15:54:13 +0700

    update code

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,13 +1,21 @@
+import UseAuth from "../hooks/UseAuth"
 
 function Index() {
+
+    const { user } = UseAuth();
+
   return (
     <div className="max-w-[80rem] mx-auto">
         <div className="flex flex-col gap-4">
-            <div className="ช้อมูล">
-            456
+            <div className="border p-4 rounded-md">
+                <h1>ข้อมูล    </h1>
             </div>
-            <div className="border p-4">
-            456
+            <div className="border p-4 rounded-md">
+                <p>{user.fullname}</p>
+                <p>{user.fullname}</p>
+                <p>{user.fullname}</p>
+                <p>{user.fullname}</p>
+                <p>{user.fullname}</p>
             </div>
         </div>
     </div>
