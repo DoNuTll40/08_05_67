31f70b77ff2e57095cd70019111477bdc0a4a76e
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-05-08 15:34:03 +0700

    upload code

---
+++ src/routes/AppRoute.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
+import Login from "../components/Login"
+import Index from "../components/Index"
+import UseAuth from "../hooks/UseAuth"
+
+const guestRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <>
+            <Outlet />
+        </>,
+        children: [
+            { index: true, element: <Login /> }
+        ]
+    }
+])
+
+const userRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <>
+            <Outlet />
+        </>,
+        children: [
+            { index: true, element: <Index /> }
+        ]
+    }
+])
+
+function AppRoute() {
+    const { user } = UseAuth();
+    console.log(user)
+    const finalRouter = user?.fullname ? userRouter : guestRouter
+    return (
+        <RouterProvider router={finalRouter} />
+    )
+}
+
+export default AppRoute
